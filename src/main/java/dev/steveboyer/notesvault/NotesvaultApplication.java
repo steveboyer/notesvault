ef551e3f5be4eb325eb025fdeccce3154a2255d3
Steve Boyer <steveboyer@users.noreply.github.com>
2026-06-22 08:30:23 -0600

    Add Spring Initializer-generated base

---
+++ src/main/java/dev/steveboyer/notesvault/NotesvaultApplication.java
@@ -0,0 +1,13 @@
+package dev.steveboyer.notesvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesvaultApplication.class, args);
+	}
+
+}
